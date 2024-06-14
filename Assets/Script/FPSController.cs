819fc142e73c1d7bd2b7fc69bdd8461f2e34d853
Khindy Zharifany <zharifanny@gmail.com>
2024-06-14 18:36:19 +0700

    add file

---
+++ Assets/Script/FPSController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class FPSController : MonoBehaviour
+{
+    public Camera playerCamera;
+    public float walkSpeed = 6.0f;
+    public float runSpeed = 10.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 45.0f;
+
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+    
+    public bool canMove = true;
+
+    CharacterController characterController;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        characterController = GetComponent<CharacterController>();
+
+        // Lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        #region Handles Movement
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        // Press Left Shift to run
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        #endregion
+
+        #region Handles Rotation
+        characterController.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * lookSpeed, 0));
+
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
+
+        #endregion
+        
+    }
+}
